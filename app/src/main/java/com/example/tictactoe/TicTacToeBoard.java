d4b488d43af0a046327a9dce921e5cf1001e7eab
SemionSolonar <shimon22361@icloud.com>
2022-12-02 10:17:35 +0200

    I created a main page, and a page for entering player names, and a starting page for creating the game

---
+++ app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
@@ -0,0 +1,67 @@
+package com.example.tictactoe;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TicTacToeBoard extends View {
+    private final int boardColor;
+    private final int XColor;
+    private final int OColor;
+    private final int winningLineColor;
+
+    private final Paint paint= new Paint();
+
+    private int cellSize= getWidth()/3;
+
+    public TicTacToeBoard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,R.styleable.TicTacToeBoard,0,0);
+
+        try {
+            boardColor =a.getInteger(R.styleable.TicTacToeBoard_boardColor,0);
+            XColor =a.getInteger(R.styleable.TicTacToeBoard_XColor,0);
+            OColor =a.getInteger(R.styleable.TicTacToeBoard_OColor,0);
+            winningLineColor =a.getInteger(R.styleable.TicTacToeBoard_winningLineColor,0);
+        }finally {
+            a.recycle();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int width, int height){
+        super.onMeasure(width,height);
+
+        int dimension = Math.min(getMeasuredWidth() , getMeasuredHeight());
+        cellSize = dimension/3;
+
+        setMeasuredDimension(dimension, dimension);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        drawGameBoard(canvas);
+    }
+
+    protected void drawGameBoard(Canvas canvas){
+        paint.setColor(boardColor);
+        paint.setStrokeWidth(16);
+
+        for (int i=1; i<3; i++){
+            canvas.drawLine(cellSize*i, 0, cellSize*i, canvas.getWidth(), paint );
+        }
+
+        for (int j=1; j<3; j++){
+            canvas.drawLine(0, cellSize*j, canvas.getWidth(), cellSize*j, paint );
+        }
+    }
+}
